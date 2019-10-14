abf93fb1840082253c3217863c6768ad65c056ef
j-ulienguyen <j-ulienguyen@hotmail.com>
2019-10-13 21:24:14 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View,Text} from 'react-native';
+import styles from './styles/AppStyles';
+
+// Import comps below
+
+
+function App(){
+	// UI
+	return (
+		<View style={styles.app}>
+			<Text>Review Package 01</Text>
+			{/* Place your comps here */}
+		</View>
+	)
+}
+
+export default App;
