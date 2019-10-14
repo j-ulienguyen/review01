8b40fd99f730f154fb317657a2c794b6e1041348
Julie Nguyen <j-ulienguyen@hotmail.com>
2019-10-13 23:42:57 -0700

    Update Main.js

--- comps/Main.js
+++ comps/Main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {View,Text} from 'react-native'
 import styles from '../styles/MainStyles';
 import Top from './Top';
-import {View} from 'react-native';
-import styles from '../styles/MainStyles';
 import Mid from './Mid';
 import Bot from './Bot'
 
@@ -11,10 +9,9 @@ function Main(){
 	// UI
 	return (
 		<View style={styles.main}>
-      <Top />
-			<Bot />
-      <Mid/>
-			<Bot/>
+      		<Top />
+      		<Mid/>
+		<Bot/>
 		</View>
 	)
 }
